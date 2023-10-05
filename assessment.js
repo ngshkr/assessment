2b2f884a35759589446a03049d8f9744fab81ce4
Hikaru Negishi <144613500+ngshkr@users.noreply.github.com>
2023-10-05 23:06:52 +0900

    bootstrap

--- assessment.js
+++ assessment.js
@@ -13,14 +13,37 @@ assessmentButton.onclick = () => {
 
     // 診断結果表示エリアの作成
     resultDivision.innerText = '';
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivision.appendChild(header);
+    // const header = document.createElement('h3');
+    // header.innerText = '診断結果';
+    // resultDivision.appendChild(header);
+
+    // const paragraph = document.createElement('p');
+    // const result = assessment(userName);
+    // paragraph.innerText = result;
+    // resultDivision.appendChild(paragraph);
+
+    //headerDivisonの作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
+    //bodyDivisonの作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
 
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivision.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
+
+    //resultDivisonにBootstrapのスタイルを適用する
+    resultDivision.setAttribute('class', 'card');
+
+    //headerDivisonとbodyDivisonをresultDivisonに差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
+
 
     // ツイートエリアの作成
     tweetDivision.innerText = '';
